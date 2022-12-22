e5941987243d655a5472bfcc2edb076c6f4f3d14
Anyikabs <anyikabsdev@gmail.com>
2022-12-21 21:17:34 -0800

    104-print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,43 +10,44 @@
  */
 void print_buffer(char *b, int size)
 {
-  int byte, index;
 
-  for (byte = 0; byte < size; byte += 10)
-    {
-      printf("%08x: ", byte);
+	int byte, index;
 
-      for (index = 0; index < 10; index++)
+
+	for (byte = 0; byte < size; byte += 10)
 	{
-	  if ((index + byte) >= size)
-	    printf("  ");
+		printf("%08x: ", byte);
 
-	  else
-	    printf("%02x", *(b + index + byte));
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf("  ");
 
-	  if ((index % 2) != 0 && index != 0)
-	    printf(" ");
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+			break;
+
+			else if (*(b + index + byte) >= 31 &&
+				*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+			printf("\n");
 	}
 
-      for (index = 0; index < 10; index++)
-	{
-	  if ((index + byte) >= size)
-	    break;
-
-	  else if (*(b + index + byte) >= 31 &&
-		   *(b + index + byte) <= 126)
-	    printf("%c", *(b + index + byte));
-
-	  else
-	    printf(".");
-	}
-
-      if (byte >= size)
-	continue;
-
-      printf("\n");
-    }
-
-  if (size <= 0)
-    printf("\n");
+	if (size <= 0)
+		printf("\n");
 }
