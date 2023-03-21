4d68fc501ea5e0e479f2777b80bcb52c2fc041c7
Anyikabs <anyikabsdev@gmail.com>
2023-03-20 20:04:44 -0700

    fixed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Anyikabs/alx-low_level_programming/tree/master/0x18-dynamic_libraries export LD_PRELOAD=/tmp/hacked.so
+wget -P /tmp https://github.com/Anyikabs/alx-low_level_programming/tree/master/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
